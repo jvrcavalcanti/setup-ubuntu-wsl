a0fb6c66ae6a9d683391f83dcdf9c81966376258
jvrcavalcanti <jonnyvictor01@gmail.com>
2022-01-13 18:13:44 -0300

    Add curl extension php -> [php8.0-curl]

--- php8-0.sh
+++ php8-0.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 sudo add-apt-repository ppa:ondrej/php
 sudo apt update
-sudo apt install php8.0 php8.0-cli php8.0-dev php8.0-xml php8.0-zip php8.0-intl php8.0-mysql php8.0-sqlite3 php8.0-gd php8.0-mbstring
+sudo apt install php8.0 php8.0-cli php8.0-dev php8.0-xml php8.0-zip php8.0-intl php8.0-mysql php8.0-sqlite3 php8.0-gd php8.0-mbstring php8.0-curl
 php -v
